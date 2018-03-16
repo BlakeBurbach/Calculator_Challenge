eb8f5c51e75b543a554a77e094781e02ce562d28
Blake Burbach <cras.bburbach@gmail.com>
2018-03-16 17:20:17 -0500

    Got the client side to send different information for each calculation.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,91 @@
 $(document).ready(readyNow);
-function readyNow(){
-    console.log('Oh Hey there!');
-    $('#calculateButton').on('click', calculate );
+
+class Calculation {
+    constructor(firstNumberIn, secondNumberIn, operatorIn){
+        this.firstNumber = firstNumberIn;
+        this.secondNumber = secondNumberIn;
+        this.operator = operatorIn;
+        this.answer = '';
+    }
 }
 
-function calculate(){
-    
+function readyNow(){
+    console.log('Oh Hey there!');
+    $('#additionBtn').on('click', setAddition );
+    $('#subtractionBtn').on('click', setSubtraction );
+    $('#multiplyBtn').on('click', setMultiply );
+    $('#divideBtn').on('click', setDivision );
+    // $('#calculateButton').on('click', sendCalculation );
 }
+
+// create calculation with addition
+function setAddition(){
+    //set the values for the calculation object being sent over
+    firstNumber = parseInt($('#firstInput').val()); 
+    secondNumber = parseInt($('#secondInput').val()); 
+    operator = 'add';
+    let calculation = new Calculation(firstNumber, secondNumber, operator);
+    sendCalculation(calculation);
+} // end setAddition
+
+// create calculation with subtraction
+function setSubtraction(){
+    //set the values for the calculation object being sent over
+    firstNumber = parseInt($('#firstInput').val()); 
+    secondNumber = parseInt($('#secondInput').val()); 
+    operator = 'subtract';
+    let calculation = new Calculation(firstNumber, secondNumber, operator);
+    sendCalculation(calculation);
+} // end setSubtraction
+
+// create calculation with multiplication
+function setMultiply(){
+    //set the values for the calculation object being sent over
+    firstNumber = parseInt($('#firstInput').val()); 
+    secondNumber = parseInt($('#secondInput').val()); 
+    operator = 'multiply';
+    let calculation = new Calculation(firstNumber, secondNumber, operator);
+    sendCalculation(calculation);
+} // end setMultiply
+
+// create calculation with division
+function setDivision(){
+    //set the values for the calculation object being sent over
+    firstNumber = parseInt($('#firstInput').val()); 
+    secondNumber = parseInt($('#secondInput').val()); 
+    operator = 'divide';
+    let calculation = new Calculation(firstNumber, secondNumber, operator);
+    sendCalculation(calculation);
+} // end setDivision
+
+// send our data over to server
+function sendCalculation(calculation){
+    $.ajax({
+        type: "POST",
+        data: calculation,
+        url: '/calculation'
+    }).done(function(response){
+        // response from server will be '200' success
+        console.log('Success');
+    }).fail(function(response){
+        console.log('Uh-oh! Something went wrong...');
+    }) // end POST
+} // end calculate
+
+// receive calculated response
+function getCalculation(){
+    $.ajax({
+        type: "GET",
+        url: "/calculation"
+    }).done(function(response){
+        appendToDom(response);
+    }); // end GET
+} // end getCalculation
+
+
+function appendToDom(response){
+    console.log(response);
+}
+
+
+// end file
--- server/server.js
+++ server/server.js
@@ -3,11 +3,26 @@ let app = express();
 const PORT = process.env.PORT || 5000;
 let bodyParse = require('body-parser');
 
+let calculationArray = [];
+
 //bring in body-parser
 app.use(bodyParse.urlencoded({extended: true}));
 
 //Serve up the static files
 app.use(express.static('server/public'));
+
+// receive client POST
+app.post('/calculation', (req, res) => {
+    let calculation = req.body;
+    console.log(calculation);
+    calculationArray.push(calculation);
+    res.sendStatus(200);
+});
+
+// respond to client POST with a GET
+app.get('/calculation', (req, res) => {
+    res.send(calculationArray);
+});
 
 //Spinning up the server
 app.listen(PORT, (req, res) => {
